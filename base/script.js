717e6c33117ea8f4b0ce18d689e6ef130cc02570
PaulKlifgt <boevo.arty@yandex.ru>
2025-02-24 16:26:14 +1000

    base enter

--- base/script.js
+++ base/script.js
@@ -3,26 +3,57 @@ const secretWord = words[Math.floor(Math.random() * words.length)];
 let attempts = 6;
 let letterStatuses = {};
 
-document.getElementById("submitGuess").addEventListener("click", processGuess);
+
 CreateKeyboard();
 CreateBoard();
 
+
 function CreateKeyboard(){
-    const keyboard = document.getElementById("keyboard");
+    
     rows = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
     for (let i = 0; i < rows.length; i++){
         row = document.createElement("div");
         row.id = String(i);
-        row.classList.add('row');
-        for (let j = 0; j < rows[i].length; j++){
-           
+        row.classList.add("row");
+        if(i==2){
+            cell_button = document.createElement("button");
+            cell_button.classList.add("cell_button");
             cell = document.createElement("div");
             cell.classList.add("cell");
+            cell.classList.add("cell-delete");
+            cell.id = "delete";
+            cell.innerText = 'DEL'
 
-            cell.innerText = rows[i][j];
+            row.appendChild(cell_button);
+            cell_button.appendChild(cell);
+        }
+        
+        for (let j = 0; j < rows[i].length; j++){
+
+            cell_button = document.createElement("button");
+            cell_button.classList.add("cell_button");
+            cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.id = rows[i][j];
+            cell.innerText = cell.id;
             
-            row.appendChild(cell);
+            row.appendChild(cell_button);
+            cell_button.appendChild(cell);
             
+        }
+
+        if(i==2){
+            cell_button = document.createElement("button");
+            cell_button.classList.add("cell_button");
+            cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.id = "enter";
+            cell.classList.add("cell-enter");
+            cell.innerText = 'ENTER'
+            cell_button.addEventListener("click", processGuess);
+
+            row.appendChild(cell_button);
+            cell_button.appendChild(cell);
         }
         
         document.getElementById("keyboard").appendChild(row);
@@ -30,6 +61,7 @@ function CreateKeyboard(){
     }  
 
 }
+
 
 function CreateBoard() {
     for (let i = 0; i < 6; i++){
@@ -45,12 +77,34 @@ function CreateBoard() {
         }
         document.getElementById("board").appendChild(row);
     }
+
+    for (let i = 0; i < 5; i++) {
+        input_letter = document.createElement("input");
+        input_letter.type = "text";
+        input_letter.maxLength = "1";
+        input_letter.classList.add("input_one");
+        cell_next = document.getElementById(String(0)+String(i));
+        input_letter.id = "inp"+cell_next.id;
+        input_letter.addEventListener('input', NextEnter);
+        cell_next.appendChild(input_letter);
+    }
+
+    let input = document.getElementById("inp00");
+    input.focus();
     
 }
 
 
 function processGuess() {
-    const guessInput = document.getElementById("guessInput").value.toLowerCase();
+
+    guessInput = '';
+    inputs = document.getElementsByClassName("input_one")
+    for (let i = 0; i < 5; i++) {
+        let input_n = inputs[i];
+        guessInput += input_n.value;
+    }
+
+    console.log(guessInput)
     if (guessInput.length !== 5) {
         alert("Слово должно состоять из 5 букв.");
         return;
@@ -70,14 +124,19 @@ function processGuess() {
         document.getElementById("message").innerText = `Вы проиграли! Загаданное слово: ${secretWord}.`;
     }
 
-    document.getElementById("guessInput").value = "";
+    
+}
+
+
+function OnKeyBoardClick(letter) {
+
 }
 
 
 function UpdateBoard(guess) {
    
     for (let i = 0; i < 5; i++) {
-        const cell = document.getElementById(String(6-attempts)+String(i))
+        cell = document.getElementById(String(6-attempts)+String(i))
         console.log(String(attempts)+String(i))
         cell.innerText = guess[i];
         if (guess[i] === secretWord[i]) {
@@ -87,6 +146,30 @@ function UpdateBoard(guess) {
         } else {
             cell.classList.add("absent");
         }
-   
+        input_letter = document.createElement("input");
+        input_letter.type = "text";
+        input_letter.maxLength = "1";
+        input_letter.classList.add("input_one");
+        input_letter.addEventListener('input', NextEnter);
+        cell_next = document.getElementById(String(6-attempts+1)+String(i));
+        input_letter.id = "inp"+cell_next.id;
+        cell_next.appendChild(input_letter);
+    }
+    let input = document.getElementById("inp"+String(6-attempts+1)+"0");
+    input.focus();
+
+}
+
+function NextEnter(e) {
+    let id = e.target.id;
+    if (Number(id.slice(-1))+1 <= 4){
+        new_id = id.slice(0, 4) + String(Number(id.slice(-1))+1);
+        let new_input = document.getElementById(new_id);
+        console.log(new_id);
+        new_input.focus();
+    }
+    else{
+        let old_input = document.getElementById(id);
+        old_input.focus();
     }
 }
